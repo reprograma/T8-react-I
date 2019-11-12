7b27f342c6c59c04ee75a99076089a75c711d402
Mell Y <pong7219@gmail.com>
2019-11-12 16:55:57 -0300

    Botao abre e fecha

--- 0-contador/src/App.js
+++ 0-contador/src/App.js
@@ -5,7 +5,8 @@ class Contador extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      contador: 0
+      contador: 0,
+      visibilidade: true
     };
   }
 
@@ -17,13 +18,47 @@ class Contador extends React.Component {
     })
   }
 
+  subtrairUm = () => {
+    this.setState(prevState => {
+      return {
+        contador: prevState.contador - 1
+      }
+    })
+  }
+
+  resetar = () => {
+    this.setState(() => {
+      return {
+        contador: 0
+      }
+    })
+  }
+
+  alternarVisibilidade = () => {
+    this.setState((estadoAnterior) => {
+      return {
+        visibilidade: !estadoAnterior.visibilidade
+      }
+    })
+  }
+
   render() {
     return (
       <div>
         <p>{this.state.contador}</p>
-        <button className="btnMaisUm" onClick={this.adicionarUm}>+1</button>
-        <button className="btnMenosUm">-1</button>
-        <button className="btnResetar">resetar</button>
+
+        <button onClick={this.alternarVisibilidade}>
+          {this.state.visibilidade === true ? "Fechar Contador" : "Abrir Contador"}
+        </button>
+
+          {this.state.visibilidade && (
+            <div>
+              <button className="btnMaisUm" onClick={this.adicionarUm}>+1</button>
+              <button className="btnMenosUm" onClick={this.subtrairUm}>-1</button>
+              <button className="btnResetar" onClick={this.resetar}>resetar</button>
+            </div>
+          )}
+
       </div>
     );
   }
